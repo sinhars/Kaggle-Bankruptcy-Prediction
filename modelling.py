790c497c1577ac88ae22be9c8b5a6cc193dd0454
Harsh V Singh <harshv.singh@outlook.com>
2021-04-28 23:58:59 +0530

    Added data exploration, cleaning, random forest and XGBoost (v1)

---
+++ modelling.py
@@ -0,0 +1,17 @@
+import sklearn
+
+
+class Modelling():
+    #Class Constructor
+    def __init__(self, vis):
+        self.np = __import__("numpy")
+        self.sklearn = __import__("sklearn")
+        self.metrics = sklearn.metrics
+        self.vis = vis
+
+    #Method to plot counts/ histogram
+    def getModelPerformance(self, trueVals, preds, figSize, plotTitle, targetNames):
+        confMatrix = self.metrics.confusion_matrix(trueVals, preds, normalize="true")
+        report = self.metrics.classification_report(trueVals, preds, target_names=targetNames)
+        print(report)
+        self.vis.plotConfusionMatrix(confMatrix, figSize=figSize, plotTitle=plotTitle)
---
+++ visualizations.py
@@ -0,0 +1,43 @@
+#Plotter class for various data exploration techniques
+class Visualizations():
+    #Class Constructor
+    def __init__(self, theme="white", fontSize=12):
+        self.np = __import__("numpy")
+        self.mpl = __import__("matplotlib")
+        self.sns = __import__("seaborn")
+        self.plt = self.mpl.pyplot
+        self.sns.set_theme(style=theme)
+        self.fontSize = fontSize
+        self.cmap = self.sns.diverging_palette(230, 20, as_cmap=True)
+
+    #Method to plot counts/ histogram
+    def plotCounts(self, xData, figSize, plotTitle):
+        self.plt.figure(figsize=figSize)
+        self.sns.countplot(x=xData)
+        self.plt.title(plotTitle, fontsize=self.fontSize)
+        self.plt.show()
+
+    def plotHistograms(self, data, figSize, bins=50):
+        data.hist(figsize=figSize, bins=bins)
+        self.plt.show()
+
+    def plotBoxPlots(self, data, figSize, plotTitle):
+        self.plt.figure(figsize=figSize)
+        ax = self.sns.boxplot(data=data, orient="h")
+        ax.set_title(plotTitle, fontsize=self.fontSize)
+        ax.set(xscale="log")
+        self.plt.show()
+
+    def plotCorrMatrix(self, data, figSize, plotTitle):
+        corrMatrix = data.corr()
+        maskMatrix = self.np.triu(self.np.ones_like(corrMatrix, dtype=bool))
+        self.plt.figure(figsize=figSize)
+        self.sns.heatmap(corrMatrix, mask=maskMatrix, cmap=self.cmap, annot=False)
+        self.plt.title(plotTitle, fontsize=self.fontSize)
+        self.plt.show()
+    
+    def plotConfusionMatrix(self, data, figSize, plotTitle):
+        self.plt.figure(figsize=figSize)
+        self.sns.heatmap(data, cmap=self.cmap, annot=True, fmt=".1%")
+        self.plt.title(plotTitle, fontsize=self.fontSize)
+        self.plt.show()
